4460385b6b7c155ba46695d4f3d2d05b1093fef4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-02 20:55:50 +0000

    Deploy akhilmhdh/mallu to github.com/akhilmhdh/mallu.git:gh-pages

---
+++ precache-manifest.d2cdf26b59b1f92a6601dc1146f706e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16d0ca091c98fa0ee79ee619758bb20",
+    "url": "/mallu/index.html"
+  },
+  {
+    "revision": "e58abae622f45dd43518",
+    "url": "/mallu/static/css/main.0db0ad62.chunk.css"
+  },
+  {
+    "revision": "2e191e46128ee2155edd",
+    "url": "/mallu/static/js/2.36a47f1e.chunk.js"
+  },
+  {
+    "revision": "74549c08325e899dd17d787a1ff374cf",
+    "url": "/mallu/static/js/2.36a47f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58abae622f45dd43518",
+    "url": "/mallu/static/js/main.bd13a588.chunk.js"
+  },
+  {
+    "revision": "4fd0068f5a4695e99df5",
+    "url": "/mallu/static/js/runtime-main.19607e3b.js"
+  },
+  {
+    "revision": "6b9acb8afdc59787a77a16a6769e6327",
+    "url": "/mallu/static/media/doodle.6b9acb8a.jpg"
+  }
+]);
